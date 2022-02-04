10f1c3693615a520372821a6cb8cfeec34278c6e
Dev Patel <63603475+Patle1234@users.noreply.github.com>
2022-02-04 08:23:29 -0600

    Initial Commit

---
+++ Shared/FindARApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindARApp.swift
+//  Shared
+//
+//  Created by Dev Patel on 2/4/22.
+//
+
+import SwiftUI
+
+@main
+struct FindARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
